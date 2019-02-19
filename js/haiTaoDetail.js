5eac7f790196db1450087d49c5cc10ac8f11b4e8
cnyoung <wy_oyc@163.com>
2019-02-19 19:31:58 +0800

    baicaijia.html测试提交

--- js/haiTaoDetail.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $.ajax({
-        url: 'http://localhost:9090/api/getmoneyctrlproduct',
-        data: { productid: 22 },
-        success: function(data) {
-            console.log(data);
-        }
-    })
-})
